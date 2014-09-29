c9a1a179c27c988996d13b75b6798d057c29ef9b
Dan Mouyard <dcmouyard@gmail.com>
2014-09-29 09:20:45 -0400

    Issue #2338395 by dcmouyard: Overrode default markup and styles of file module.

--- template.php
+++ template.php
@@ -14,6 +14,7 @@ require_once dirname(__FILE__) . '/includes/entity.inc';
 require_once dirname(__FILE__) . '/includes/feed-icon.inc';
 require_once dirname(__FILE__) . '/includes/field.inc';
 require_once dirname(__FILE__) . '/includes/field-collection.inc';
+require_once dirname(__FILE__) . '/includes/file.inc';
 require_once dirname(__FILE__) . '/includes/form.inc';
 require_once dirname(__FILE__) . '/includes/html.inc';
 require_once dirname(__FILE__) . '/includes/js.inc';
