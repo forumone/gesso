193a8527ccde3f8c5cf614b6098c8ebf87ae652b
Corey Lafferty <clafferty@forumone.com>
2016-06-14 15:21:43 -0400

    updates panels and layout classes to match naming conventions

--- panels/layouts/sidebar/sidebar.tpl.php
+++ panels/layouts/sidebar/sidebar.tpl.php
@@ -1,25 +1,48 @@
 <?php
-  $preface = (empty($content['preface']) ? 'preface-empty' : 'preface');
-  $main = (empty($content['main']) ? 'main-empty' : 'main');
-  $sidebar = (empty($content['sidebar']) ? 'sidebar-empty' : 'sidebar');
-  $classes = "$preface $main $sidebar";
-?>
+/**
+ * @file
+ * Template for the sidebar panels layout.
+ */
 
-<div<?php if (!empty($css_id)): ?> id="<?php print $css_id; ?>"<?php endif; ?> class="layout-panels-sidebar <?php print $classes; ?>">
-  <?php if (!empty($content['preface'])): ?>
-    <div class="layout-preface">
-      <?php print $content['preface']; ?>
-    </div>
-  <?php endif; ?>
-  <?php if (!empty($content['main'])): ?>
-    <div class="layout-main">
-      <?php print $content['main']; ?>
-    </div>
-  <?php endif; ?>
-  <?php if (!empty($content['sidebar'])): ?>
-    <aside class="layout-sidebar">
-      <?php print $content['sidebar']; ?>
-    </aside>
-  <?php endif; ?>
+$wrapper_attributes = array('class' => array('layout-sidebar'));
+$preface_open = '';
+$preface_close = '';
+$main_open = '';
+$main_close = '';
+$sidebar_open = '';
+$sidebar_close = '';
+
+if (!empty($css_id)) {
+  $wrapper_attributes['id'] = $css_id;
+}
+
+if (!empty($content['preface'])) {
+  $preface_open = '<div class="layout-sidebar__header">';
+  $preface_close = '</div>';
+}
+
+if (!empty($content['main'])) {
+  $main_open = '<div class="layout-sidebar__main">';
+  $main_close = '</div>';
+}
+
+if (!empty($content['sidebar'])) {
+  $wrapper_attributes['class'][] = 'has-sidebar';
+  $sidebar_open = '<div class="layout-sidebar__sidebar">';
+  $sidebar_close = '</div>';
+}
+?>
+<div<?php print drupal_attributes($wrapper_attributes); ?>>
+  <?php print $preface_open; ?>
+    <?php print $content['preface']; ?>
+  <?php print $preface_close; ?>
+
+  <?php print $main_open; ?>
+    <?php print $content['main']; ?>
+  <?php print $main_close; ?>
+
+  <?php print $sidebar_open; ?>
+    <?php print $content['sidebar']; ?>
+  <?php print $sidebar_close; ?>
 </div>
 
