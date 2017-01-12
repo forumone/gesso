02cff717e8cc7b7eb4dfa63e91cf7a6871959a03
Corey Lafferty <coreylafferty@gmail.com>
2017-01-12 13:49:24 -0500

    updated menu classes and added mobile menu js to global library

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,7 +12,7 @@
       var $mobileNav = $('<nav class="mobile-menu" role="navigation"></nav>'),
           $mobileBar = $('<div class="mobile-menu__bar"><button class="mobile-menu__button js-mobile-menu-button mobile-menu__button--menu"><span class="mobile-menu__icon mobile-menu__icon--menu">Menu</span></button></div>'),
           $mobileLinks = $('<div class="mobile-menu__links hidden"></div>'),
-          $mainMenu = $('.region-navigation', context).find('.nav--main-menu, .block--system-main-menu .nav, .block--superfish .sf-menu').not('.contextual-links').first().clone(),
+          $mainMenu = $('.region-navigation', context).find('.nav--main, .block--system-main-menu .nav, .block--superfish .sf-menu').not('.contextual-links').first().clone(),
           $isSuperfish = ($mainMenu.hasClass('sf-menu')) ? true : false;
 
       // Only create mobile menu if there is a main menu.
