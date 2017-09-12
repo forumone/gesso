93575959c598af1018cf32b27cb4a8021045ee52
Corey Lafferty <coreylafferty@gmail.com>
2017-09-12 17:57:53 -0400

    initial reorg test

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -2,11 +2,11 @@ module.exports = function (grunt) {
   grunt.config.merge({
     watch: {
       gesso: {
-        files: ['<%= pkg.themePath %>/sass/**/*.scss','<%= pkg.themePath %>/pattern-lab/source/_patterns/**/*.scss'],
+        files: ['<%= pkg.themePath %>/pattern-lab/source/**/*.scss','<%= pkg.themePath %>/pattern-lab/source/_patterns/**/*.scss'],
         tasks: ['gessoBuildStyles'],
       },
       patternlab: {
-        files: ['<%= pkg.themePath %>/pattern-lab/source/**/*.twig','<%= pkg.themePath %>/pattern-lab/source/**/*.json','<%= pkg.themePath %>/pattern-lab/source/**/*.yaml'],
+        files: ['<%= pkg.themePath %>/pattern-lab/source/**/*.twig','<%= pkg.themePath %>/pattern-lab/source/**/*.json','<%= pkg.themePath %>/pattern-lab/source/**/*.yaml','<%= pkg.themePath %>/pattern-lab/source/**/*.yml'],
         tasks: ['shell:patternlab'],
         options: {
           livereload: true
