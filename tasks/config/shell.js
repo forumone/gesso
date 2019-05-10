aeb92a9511ecf16bdb81eae8c859457ceb4db795
Corey Lafferty <coreylafferty@gmail.com>
2019-05-09 23:11:22 -0400

    initial test of node pattern lab v3

--- tasks/config/shell.js
+++ tasks/config/shell.js
@@ -1,24 +1,8 @@
 module.exports = function (grunt) {
   grunt.config.merge({
     shell: {
-      patternlabSetupInstall: {
-        command: "composer create-project pattern-lab/edition-drupal-standard pattern-lab --no-interaction || true",
-        options: {
-          execOptions : {
-            cwd: '<%= pkg.themePath %>'
-          }
-        }
-      },
-      patternlabComposer: {
-        command: 'composer install',
-        options: {
-          execOptions : {
-            cwd: '<%= pkg.themePath %>/pattern-lab'
-          }
-        }
-      },
       patternlab: {
-        command: 'php core/console --generate',
+        command: 'patternlab build --config ./patternlab-config.json',
         options: {
           execOptions: {
             cwd: '<%= pkg.themePath %>/pattern-lab'
