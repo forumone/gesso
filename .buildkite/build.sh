94a676cb82dbb733ef56c2ff43f0b645a4659a72
gustavderdrache <aford@forumone.com>
2019-07-03 17:00:46 -0400

    --set is implied

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -4,9 +4,9 @@ set -exuo pipefail
 
 ssh-keyscan git.drupal.org >> ~/.ssh/known_hosts
 
-git config --set user.name "$GIT_USER"
-git config --set user.email "$GIT_EMAIL"
+git config user.name "$GIT_USER"
+git config user.email "$GIT_EMAIL"
 
-git config --set remote.drupal.url git@git.drupal.org:project/gesso.git
+git config remote.drupal.url git@git.drupal.org:project/gesso.git
 
 git push drupal HEAD:"$BUILDKITE_BRANCH"
