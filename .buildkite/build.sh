0a540e768f24b78083bfc5508c0c4559f37c4848
gustavderdrache <aford@forumone.com>
2019-07-03 16:43:55 -0400

    Remove unneeded git fetch

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -9,6 +9,4 @@ git config --set user.email "$GIT_EMAIL"
 
 git config --set remote.drupal.url git@git.drupal.org:project/gesso.git
 
-git fetch drupal "$BUILDKITE_BRANCH"
-
 git push drupal HEAD:"$BUILDKITE_BRANCH"
