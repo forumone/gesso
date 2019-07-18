370832365c0b4071417ffe7b77cfbc13e2294cdf
Stephen Lucero <stephen.lucero@mediacurrent.com>
2019-07-18 17:08:24 -0400

    Explicitly target the remote branch.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -14,4 +14,4 @@ git config remote.drupal.url 'git@git.drupal.org:project/gesso.git'
 
 # Push the latest updates to a branch of the same name.
 # Temporary: Add the dry-run flag to prevent actually pushing changes.
-git push --dry-run drupal "$BUILDKITE_BRANCH"
+git push --dry-run drupal "$BUILDKITE_BRANCH":"$BUILDKITE_BRANCH"
