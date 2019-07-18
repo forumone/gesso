d5773e6fac94e6b3d372147cc6bf5802f4abe73d
Stephen Lucero <stephen.lucero@mediacurrent.com>
2019-07-18 16:11:56 -0400

    Add buildkite build script.

---
+++ .buildkite/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+# Ensure a host verification prompt doesn't prevent the connection.
+ssh-keyscan git.drupal.org >> ~/.ssh/known_hosts
+
+# Assign the expected committer details.
+git config user.name "$GIT_USER"
+git config user.email "$GIT_EMAIL"
+
+# Add the remote for the drupal.org repository mirror.
+git remote add drupal 'git@git.drupal.org:project/gesso.git'
+
+# Push the latest updates to a branch of the same name.
+# Temporary: Add the dry-run flag to prevent actually pushing changes.
+git push --dry-run drupal "$BUILDKITE_BRANCH"
