7d30ff5d4b2b27b0ba9625cb1c492f225fc53aed
Stephen Lucero <stephen.lucero@mediacurrent.com>
2019-07-19 10:12:07 -0400

    Skip local branch checkout.
    Push the branch reference tracked for the origin repo directly to the
    mirrored repository rather than checking out a local trancking branch
    and pushing it.
    
    This should also add more flexibility for pushing other references aside
    from just branches.

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -12,6 +12,9 @@ git config user.email "$GIT_EMAIL"
 # Add the remote for the drupal.org repository mirror.
 git config remote.drupal.url 'git@git.drupal.org:project/gesso.git'
 
+# Update our list of remote branches.
+git fetch --depth=5 drupal
+
 # Push the latest updates to a branch of the same name.
 # Temporary: Add the dry-run flag to prevent actually pushing changes.
-git push --dry-run drupal "$BUILDKITE_BRANCH":"$BUILDKITE_BRANCH"
+git push --dry-run drupal origin/"$BUILDKITE_BRANCH":refs/remotes/drupal/"$BUILDKITE_BRANCH"
