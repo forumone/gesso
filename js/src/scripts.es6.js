b9eafa067146558b49ea4fd5e01b78de04811c11
Kelli Monahan <kmonahan@forumone.com>
2019-09-06 16:07:11 -0500

    Fix typo and remove demo code

--- js/src/scripts.es6.js
+++ js/src/scripts.es6.js
@@ -1,14 +1,11 @@
 // Custom scripts file
-
-import MenuBar from "./modules/_MenuBar.es6";
-
 (function () {
 
   'use strict';
 
   // Generic function that runs on window resize.
   function resizeStuff() {
-    const foo = new MenuBar();
+
   }
 
   // Runs function once on window resize.
