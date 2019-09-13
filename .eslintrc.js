6388a32cbbf6577f9a42f8f4913dcce4f3ae5578
Kelli Monahan <kmonahan@forumone.com>
2019-09-13 15:57:44 -0500

    Remove rules included in prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     "browser": true
   },
   "plugins": [
-    "prettier",
     "implicit-dependencies"
   ],
   "extends": [
@@ -26,21 +25,14 @@ module.exports = {
     "constructor-super": "error",
     "curly": "error",
     "dot-notation": "error",
-    "eol-last": "off",
     "eqeqeq": "error",
     "guard-for-in": "error",
     "implicit-dependencies/no-implicit": ["error", { optional: true }],
-    "linebreak-style": "off",
-    "new-parens": "off",
-    "newline-per-chained-call": "off",
     "no-console": "error",
     "no-empty": "error",
     "no-empty-function": "error",
-    "no-extra-semi": "off",
     "no-fallthrough": "error",
-    "no-floating-decimal": "error",
     "no-irregular-whitespace": "off",
-    "no-multiple-empty-lines": "off",
     "no-param-reassign": "error",
     "no-sparse-arrays": "error",
     "no-template-curly-in-string": "error",
@@ -51,15 +43,9 @@ module.exports = {
     "one-var": ["error", "never"],
     "prefer-const": "error",
     "prefer-template": "error",
-    "prettier/prettier": "error",
-    "quote-props": [
-      "error",
-      "as-needed"
-    ],
     "sort-imports": ["error", {
       "ignoreCase": true,
       "ignoreDeclarationSort": true,
     }],
-    "space-before-function-paren": "off",
   }
 };
