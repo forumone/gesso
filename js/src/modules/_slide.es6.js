7261dfdd1a63fa7db88e612c755d54dff94bf3dd
Alessia Pizzoccheri <apizzoccheri@forumone.com>
2020-03-24 12:18:49 -0400

    added slidein, slideout and toggleslide

---
+++ js/src/modules/_slide.es6.js
@@ -0,0 +1,81 @@
+/**
+ * Function: slideUp. Slide target element out view
+ * @param  target the element sliding up
+ * @param  duration the duration of the animation
+ */
+export const slideUp = (target, duration = 500) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = `${duration}ms`;
+  target.style.boxSizing = 'border-box';
+  target.style.height = `${target.offsetHeight}px`;
+  target.style.overflow = 'hidden';
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(() => {
+    target.style.height = 0;
+  }, 10);
+  window.setTimeout(() => {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+};
+
+/**
+ * Function: slideDown. Slide target element into view
+ * @param  target the element sliding down
+ * @param  duration the duration of the animation
+ */
+export const slideDown = (target, duration = 500) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+  if (display === 'none') {
+    display = 'block';
+  }
+  target.style.display = display;
+  const height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = `${duration}ms`;
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout(() => {
+    target.style.height = `${height}px`;
+  }, 10);
+  window.setTimeout(() => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+};
+
+/**
+ * Function: slideToggle. Toggle element between slideDonw
+ * and slideDown
+ * @param  target the element to toggle
+ * @param  duration the duration of the animation
+ */
+export const slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+};
