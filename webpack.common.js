4aa5aa41fc823cc0c0590138c44d3997f52d9cb0
KJ Monahan <kmonahan@forumone.com>
2021-06-05 17:05:24 -0500

    Initial webpack setup

---
+++ webpack.common.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+module.exports = {
+  entry: () => {
+    // Grab any JS files.
+    const jsFiles = glob.sync('source/**/!(*.stories).js').reduce((entries, currentFile) => {
+      const fileName = path.basename(currentFile.replace('source/', ''), '.js');
+      entries[`js/${fileName}`] = path.resolve(__dirname, currentFile);
+      return entries;
+    }, {});
+    // Grab any SCSS files that aren't prefixed with _.
+    const scssFiles = glob.sync('source/**/*.scss', {
+      ignore: [
+        '**/_*'
+      ]
+    }).reduce((entries, currentFile) => {
+      const fileName = path.basename(currentFile.replace('source/', ''), '.scss');
+      entries[`css/${fileName}`] = `./${currentFile}`;
+      return entries;
+    }, {});
+    return {
+      ...jsFiles,
+      ...scssFiles
+    };
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new RemovePlugin({
+      after: {
+        test: [
+          {
+            folder: './dist/css',
+            method: absolutePath => {
+              return new RegExp(/\.js(\.map)?$/, 'm').test(absolutePath);
+            }
+          }
+        ]
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
