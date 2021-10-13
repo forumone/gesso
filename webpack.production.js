74b00a912bda04854a6942f520d1f71efaf0dfe0
KJ Monahan <kj@forumone.com>
2021-10-13 09:10:22 -0500

    Port chunk settings and common.js fix from Gesso

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,24 @@
 const { merge } = require('webpack-merge');
+const TerserJsPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'js/common',
+      minChunks: 2,
+    },
+    minimizer: [
+      new TerserJsPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  devtool: false,
 });
