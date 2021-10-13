74232fa21a4e2c274237357b0f1682db491fab2c
Dan Mouyard <dcmouyard@gmail.com>
2021-10-13 15:24:59 -0400

    Fix accessibility of back to top component

--- js/src/modules/_back-to-top.es6.js
+++ js/src/modules/_back-to-top.es6.js
@@ -10,17 +10,20 @@ export default function (threshold = 200, smoothScroll = true) {
   if (backToTop) {
     if (!isNaN(threshold)) {
       backToTop.setAttribute('aria-hidden', 'true');
+      backToTop.setAttribute('tabIndex', '-1');
       const scrollHandler = () => {
         if (
           window.scrollY >= threshold &&
           backToTop.getAttribute('aria-hidden') === 'true'
         ) {
           backToTop.setAttribute('aria-hidden', 'false');
+          backToTop.removeAttribute('tabIndex');
         } else if (
           window.scrollY < threshold &&
           backToTop.getAttribute('aria-hidden', 'false')
         ) {
           backToTop.setAttribute('aria-hidden', 'true');
+          backToTop.setAttribute('tabIndex', '-1');
         }
       };
       let stillScrolling = false;
