b20657500779010b744c44da7abb7c2059ad952e
Dan Mouyard <dcmouyard@gmail.com>
2021-10-15 14:49:05 -0400

    Add unique_id Twig filter

---
+++ gesso_helper/src/TwigExtension/UniqueIdTwigExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Gesso theme twig extension for creating a unique ID.
+ */
+namespace Drupal\gesso_helper\TwigExtension;
+
+use Drupal\Component\Utility\Html;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class UniqueIdTwigExtension extends AbstractExtension {
+  /**
+   * @inheritdoc
+   */
+  public function getName() {
+    return 'gesso_helper_unique_id';
+  }
+
+  public function getFilters() {
+    $filters = parent::getFilters();
+    $filters[] = new TwigFilter('unique_id', '\Drupal\Component\Utility\Html::getUniqueId');
+    return $filters;
+  }
+
+}
