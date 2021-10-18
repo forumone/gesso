5b219a8508c1136be1df5f4b1f2fe1cc21d8db64
KJ Monahan <kj@forumone.com>
2021-10-18 09:01:51 -0500

    Include accessibility fix from 4.x-RC

--- source/03-components/back-to-top/back-to-top.es6.js
+++ source/03-components/back-to-top/back-to-top.es6.js
@@ -9,17 +9,20 @@ Drupal.behaviors.backToTop = {
     if (backToTop) {
       if (!Number.isNaN(threshold)) {
         backToTop.setAttribute('aria-hidden', 'true');
+        backToTop.setAttribute('tabIndex', '-1');
         const scrollHandler = () => {
           if (
             window.scrollY >= threshold &&
             backToTop.getAttribute('aria-hidden') === 'true'
           ) {
             backToTop.setAttribute('aria-hidden', 'false');
+            backToTop.removeAttribute('tabIndex');
           } else if (
             window.scrollY < threshold &&
             backToTop.getAttribute('aria-hidden', 'false')
           ) {
             backToTop.setAttribute('aria-hidden', 'true');
+            backToTop.setAttribute('tabIndex', '-1');
           }
         };
         let stillScrolling = false;
