426d0118aa2b3b62a62c13d089dfdf8c256eca44
KJ Monahan <kj@forumone.com>
2021-10-20 14:46:22 -0500

    Inject dependencies properly

--- gesso_helper/src/Plugin/Field/FieldFormatter/GessoButtonFormatter.php
+++ gesso_helper/src/Plugin/Field/FieldFormatter/GessoButtonFormatter.php
@@ -3,9 +3,15 @@
 namespace Drupal\gesso_helper\Plugin\Field\FieldFormatter;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'Gesso Button' formatter.
@@ -19,6 +25,71 @@ use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
  * )
  */
 class GessoButtonFormatter extends LinkFormatter {
+
+  /**
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
+   * Constructs a new LinkFormatter.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   *   The path validator service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory service.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $themeManager
+   *   The theme manager service.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, PathValidatorInterface $path_validator, RendererInterface $renderer, ConfigFactoryInterface $configFactory, ThemeManagerInterface $themeManager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $path_validator);
+    $this->renderer = $renderer;
+    $this->configFactory = $configFactory;
+    $this->themeManager = $themeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('path.validator'),
+      $container->get('renderer'),
+      $container->get('config.factory'),
+      $container->get('theme.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -35,9 +106,7 @@ class GessoButtonFormatter extends LinkFormatter {
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $elements = parent::settingsForm($form, $form_state);
-    // TODO: Convert to dependency injection
-    $renderer = \Drupal::service('renderer');
-    $defaultThemeConfig = \Drupal::config('system.theme');
+    $defaultThemeConfig = $this->configFactory->get('system.theme');
     $themeName = $defaultThemeConfig->get('default');
     $sizes = array_values($this->getSizes($themeName));
     $styles = array_values($this->getStyles($themeName));
@@ -63,9 +132,9 @@ class GessoButtonFormatter extends LinkFormatter {
       '#options' => $styleOptions,
       '#default_value' => $this->getSetting('button_style'),
     ];
-    $themeConfig = \Drupal::config($themeName . '.settings');
-    $renderer->addCacheableDependency($elements, $themeConfig);
-    $renderer->addCacheableDependency($elements, $defaultThemeConfig);
+    $themeConfig = $this->configFactory->get($themeName . '.settings');
+    $this->renderer->addCacheableDependency($elements, $themeConfig);
+    $this->renderer->addCacheableDependency($elements, $defaultThemeConfig);
 
     return $elements;
   }
@@ -84,9 +153,7 @@ class GessoButtonFormatter extends LinkFormatter {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = parent::viewElements($items, $langcode);
     $settings = $this->getSettings();
-    // TODO: Convert to dependency injection
-    $renderer = \Drupal::service('renderer');
-    $themeName = \Drupal::service('theme.manager')->getActiveTheme()->getName();
+    $themeName = $this->themeManager->getActiveTheme()->getName();
     $sizes = array_flip($this->getSizes($themeName));
     $styles = array_flip($this->getStyles($themeName));
 
@@ -108,12 +175,20 @@ class GessoButtonFormatter extends LinkFormatter {
         }
       }
     }
-    $themeConfig = \Drupal::config($themeName . '.settings');
-    $renderer->addCacheableDependency($element, $themeConfig);
+    $themeConfig = $this->configFactory->get($themeName . '.settings');
+    $this->renderer->addCacheableDependency($element, $themeConfig);
 
     return $element;
   }
 
+  /**
+   * Get the button sizes from a given theme's settings.
+   * @param string $theme
+   *   The machine name of the theme.
+   *
+   * @return array
+   *   The set of sizes, keyed by CSS classes.
+   */
   private function getSizes($theme) {
     $sizes = [];
     $theme_sizes = theme_get_setting('button_sizes', $theme) ?? "button|Standard\nbutton.button--small|Small\nbutton.button--large|Large";
@@ -127,6 +202,14 @@ class GessoButtonFormatter extends LinkFormatter {
     return $sizes;
   }
 
+  /**
+   * Get the button styles from a given theme's settings.
+   * @param string $theme
+   *   The machine name of the theme.
+   *
+   * @return array
+   *   The set of styles, keyed by CSS classes.
+   */
   private function getStyles($theme) {
     $styles = [];
     $theme_styles = theme_get_setting('button_styles', $theme) ?? "button|Primary\nbutton.button--secondary|Secondary\nbutton.button--danger|Danger";
