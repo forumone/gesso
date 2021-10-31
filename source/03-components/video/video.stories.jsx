0d4bdfb95db185a6eca5adeeaab37946305f2176
Dan Mouyard <dcmouyard@gmail.com>
2021-10-31 16:07:11 -0400

    Refactor video component to accomodate both local and remote videos

--- source/03-components/video/video.stories.jsx
+++ source/03-components/video/video.stories.jsx
@@ -3,17 +3,25 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './video.twig';
 import data from './video.yml';
+import localData from './video--local.yml';
 
 const settings = {
   title: 'Components/Video',
 };
 
-const Video = args => (
+const RemoteVideo = args => (
   parse(twigTemplate({
     ...args,
   }))
 );
-Video.args = { ...data };
+RemoteVideo.args = { ...data };
+
+const LocalVideo = args => (
+  parse(twigTemplate({
+    ...args,
+  }))
+);
+LocalVideo.args = { ...localData };
 
 export default settings;
-export { Video };
+export { RemoteVideo, LocalVideo };
