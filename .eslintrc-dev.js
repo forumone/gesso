0c600946390c8c4347a43786f29110a3ec726a2c
KJ Monahan <kj@forumone.com>
2021-11-01 15:09:55 -0500

    Replace eslint-loader and add dev-specific linting rules

---
+++ .eslintrc-dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: '.eslintrc.js',
+  rules: {
+    'no-console': 'off',
+    'no-empty-function': 'off',
+    'no-unused-vars': 'off',
+    'prefer-const': 'off',
+  },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,15 @@
 const { merge } = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  plugins: [
+    new ESLintPlugin({
+      overrideConfigFile: path.resolve(__dirname, '.eslintrc-dev.js'),
+      useEslintrc: false,
+    }),
+  ],
 });
