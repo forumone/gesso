eaeb997f1e231c5255397db0defc61a914308ac2
Dan Mouyard <dcmouyard@gmail.com>
2021-11-01 20:28:15 -0400

    Add views-view-list to Storybook

---
+++ source/03-components/view/views-view-list/views-view-list.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import parse from 'html-react-parser';
+
+import twigTemplate from './views-view-list.twig';
+import data from './views-view-list.yml';
+
+const settings = {
+  title: 'Components/Views/List',
+};
+
+const List = args => (
+  parse(twigTemplate({
+    ...args,
+  }))
+);
+List.args = { ...data };
+
+export default settings;
+export { List };
