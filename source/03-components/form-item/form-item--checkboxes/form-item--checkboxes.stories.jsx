8856d89769547298df30299fe4186e1ea2e32abf
KJ Monahan <kj@forumone.com>
2021-11-17 16:30:35 -0600

    Sync Storybook and Drupal for fieldset and checkboxes

--- source/03-components/form-item/form-item--checkboxes/form-item--checkboxes.stories.jsx
+++ source/03-components/form-item/form-item--checkboxes/form-item--checkboxes.stories.jsx
@@ -1,17 +1,49 @@
 import parse from 'html-react-parser';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
+import fieldsetTwigTemplate from '../../fieldset/fieldset.twig';
 import twigTemplate from './form-item--checkboxes.twig';
+import { Checkbox } from '../form-item--checkbox/form-item--checkbox.stories.jsx';
 import data from './form-item--checkboxes.yml';
 
 const settings = {
   title: 'Components/Form Item/Checkboxes',
 };
 
-const Checkboxes = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
+const checkboxesChildren = ReactDOMServer.renderToStaticMarkup(
+  <>
+    {Checkbox({
+      ...Checkbox.args,
+      title: 'Choice A',
+    })}
+    {Checkbox({
+      ...Checkbox.args,
+      title: 'Choice B',
+    })}
+    {Checkbox({
+      ...Checkbox.args,
+      title: 'Choice C',
+    })}
+    {Checkbox({
+      ...Checkbox.args,
+      title: 'Choice D',
+    })}
+  </>
 );
+
+const fieldsetChildren = twigTemplate({
+  children: checkboxesChildren,
+});
+
+const Checkboxes = args =>
+  parse(
+    fieldsetTwigTemplate({
+      ...args,
+      modifier_classes: 'fieldset--checkboxes',
+      children: fieldsetChildren,
+    })
+  );
 Checkboxes.args = { ...data };
 
 export default settings;
