d49ede9c396b5fb8e58b43a65d22178fdfd26fe0
KJ Monahan <kj@forumone.com>
2021-11-18 08:14:48 -0600

    Sync Storybook and Drupal for select form items

--- source/03-components/form-item/form-item--select/form-item--select.stories.jsx
+++ source/03-components/form-item/form-item--select/form-item--select.stories.jsx
@@ -1,6 +1,8 @@
 import parse from 'html-react-parser';
 
 import twigTemplate from './form-item--select.twig';
+import selectTemplate from './_select.twig';
+import labelTemplate from '../_form-item-label.twig';
 import data from './form-item--select.yml';
 import withGroupsData from './form-item--select-with-groups.yml';
 
@@ -8,19 +10,26 @@ const settings = {
   title: 'Components/Form Item/Select',
 };
 
-const Default = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const select = args => selectTemplate(args);
+const label = args => labelTemplate(args);
+const Default = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      label: label(args),
+      children: select(args),
+    })
+  );
 Default.args = { ...data };
 
-
-const WithGroups = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const WithGroups = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      label: label(args),
+      children: select(args),
+    })
+  );
 WithGroups.args = { ...withGroupsData };
 
 export default settings;
