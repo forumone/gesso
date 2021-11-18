aa6339338288539d76774f6b782787b889b03a82
KJ Monahan <kj@forumone.com>
2021-11-18 09:22:36 -0600

    Sync Drupal and Storybook for textareas

--- source/03-components/form-item/form-item--textarea/form-item--textarea.stories.jsx
+++ source/03-components/form-item/form-item--textarea/form-item--textarea.stories.jsx
@@ -1,17 +1,24 @@
 import parse from 'html-react-parser';
 
-import twigTemplate from './form-item--textarea.twig';
+import twigTemplate from '../form-item.twig';
+import textareaTemplate from './_textarea.twig';
+import labelTemplate from '../_form-item-label.twig';
 import data from './form-item--textarea.yml';
 
 const settings = {
   title: 'Components/Form Item/Textarea',
 };
 
-const Textarea = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const textarea = args => textareaTemplate(args);
+const label = args => labelTemplate(args);
+const Textarea = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      label: label(args),
+      children: textarea(args),
+    })
+  );
 Textarea.args = { ...data };
 
 export default settings;
