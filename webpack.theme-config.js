6c554390ff397c2dd6c19b06bcec6e41073668fc
KJ Monahan <kj@forumone.com>
2022-10-17 10:29:07 -0500

    [616] Fix infinite rebuild when updating design tokens

--- webpack.theme-config.js
+++ webpack.theme-config.js
@@ -1,9 +1,22 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const path = require('path');
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'production',
   entry: {
     'design-tokens': './source/00-config/config.design-tokens.yml',
   },
-});
+  context: __dirname,
+  module: {
+    rules: [
+      {
+        test: /config\.design-tokens\.yml$/,
+        exclude: /node_modules/,
+        use: [path.resolve(__dirname, './lib/configLoader.js')],
+        type: 'asset/source',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
