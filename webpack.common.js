9ab8680f81c0a5de51f4fddd198099bba8e073b7
KJ Monahan <kj@forumone.com>
2023-01-18 10:03:15 -0600

    Update Gesso for node 16

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
-const dartSass = require('sass');
+const embeddedSass = require('sass-embedded');
 
 module.exports = {
   entry: () => {
@@ -120,7 +120,8 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
-              implementation: dartSass,
+              implementation: embeddedSass,
+              webpackImporter: false,
               sassOptions: {
                 includePaths: [path.resolve(__dirname, 'source')],
               },
