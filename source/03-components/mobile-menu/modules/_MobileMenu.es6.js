e2df669eac410a3bcd8616243cec944dbd4595a1
Dan Mouyard <dcmouyard@gmail.com>
2023-02-22 15:24:23 -0500

    Fix duplicate IDs in mobile menu

--- source/03-components/mobile-menu/modules/_MobileMenu.es6.js
+++ source/03-components/mobile-menu/modules/_MobileMenu.es6.js
@@ -76,6 +76,14 @@ class MobileMenu extends OverlayMenu {
     if (blockClone.id) {
       blockClone.id = `${blockClone.id}-mobile`;
     }
+    const childrenWithId = [...blockClone.querySelectorAll('[id]')];
+    if (blockClone.matches('[id]')) {
+      childrenWithId.push(blockClone);
+    }
+    childrenWithId.forEach((e) => {
+      e.id += '-mobile';
+    });
+
     return blockClone;
   }
 
