b08a4a70395e5d81d4b2e5fa7a639486f445c2dd
Dan Mouyard <dcmouyard@gmail.com>
2023-03-08 15:30:41 -0500

    Refactor accordion component

--- source/03-components/accordion/accordion.stories.jsx
+++ source/03-components/accordion/accordion.stories.jsx
@@ -2,24 +2,34 @@ import parse from 'html-react-parser';
 
 import accordionTemplate from './accordion.twig';
 import accordionItemTemplate from './accordion-item.twig';
+import globalData from '../../00-config/storybook.global-data.yml';
 import data from './accordion.yml';
 import './accordion.scss';
 import './accordion.es6';
 
-const accordionData = data.accordions;
-const accordions = accordionData.map(item => accordionItemTemplate(item)).join('');
-
 const settings = {
-  title: 'Components/Accordion'
+  title: 'Components/Accordion',
+  parameters: {
+    controls: {
+      include: [
+        'modifier_classes',
+        'allow_multiple',
+      ],
+    }
+  },
 };
 
-const Accordion = args => (
-  parse(accordionTemplate({
-    accordion_items: accordions,
-    ...args,
-  }))
-);
-Accordion.args = { ...data };
+const Accordion = args => {
+  const accordionItems = (args.accordion_data || data.accordion_data).map(item => accordionItemTemplate(item, args)).join('');
+
+  return (
+    parse(accordionTemplate({
+      accordion_items: accordionItems,
+      ...args,
+    }))
+  );
+}
+Accordion.args = { ...globalData, ...data };
 
 export default settings;
 export { Accordion };
