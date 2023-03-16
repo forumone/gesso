a9203046ea1d9007893e1ad57895f47f45015dfc
Dan Mouyard <dcmouyard@gmail.com>
2023-03-16 11:22:37 -0400

    Use once() for overlay-menu script

--- source/03-components/overlay-menu/overlay-menu.es6.ts
+++ source/03-components/overlay-menu/overlay-menu.es6.ts
@@ -1,10 +1,11 @@
 import Drupal from 'drupal';
+import once from 'once';
 import OverlayMenu from './modules/_OverlayMenu.es6';
 
 Drupal.behaviors.overlayMenu = {
   attach(context) {
     const menus: NodeListOf<HTMLElement> =
-      context.querySelectorAll('.c-overlay-menu');
+      once('overlay-menu', '.c-overlay-menu', context);
 
     menus.forEach(menu => {
       const overlayMenu = new OverlayMenu(menu);
