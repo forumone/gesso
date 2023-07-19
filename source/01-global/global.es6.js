f71d31dc51b22277cc1bdb384737a09834736178
Corey Lafferty <coreylafferty@gmail.com>
2023-07-19 14:07:46 -0400

    made adjustment to this so it works in storybook

--- source/01-global/global.es6.js
+++ source/01-global/global.es6.js
@@ -1,4 +1,4 @@
 if (document.documentElement.classList.contains('no-js')) {
-  document.documentElement.classList.add('js');
   document.documentElement.classList.remove('no-js');
 }
+document.documentElement.classList.add('js');
