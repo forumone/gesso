722575ac314933b7e52aa5ae1dc66fcd38b63826
Dan Mouyard <dcmouyard@gmail.com>
2023-07-26 15:41:44 -0400

    Update responsive table script to only add wrapper if it doesn’t already have one

--- source/03-components/wysiwyg/wysiwyg.es6.js
+++ source/03-components/wysiwyg/wysiwyg.es6.js
@@ -8,23 +8,28 @@ Drupal.behaviors.wysiwyg = {
       // Wrap all tables output via WYSIWYG with a responsive table layout.
       const tables = wysiwyg.querySelectorAll('[data-wysiwyg] table');
       tables.forEach((table, i) => {
-        const responsiveTable = document.createElement('div');
-        const caption = table.querySelector('caption');
+        const tableParent = table.parentNode;
 
-        responsiveTable.classList.add('l-responsive-table');
-        responsiveTable.setAttribute('tabindex', '0');
-        responsiveTable.setAttribute('role', 'region');
+        // Don’t add wrapper if it already has one.
+        if (!tableParent.classList.contains('l-responsive-table')) {
+          const responsiveTable = document.createElement('div');
+          const caption = table.querySelector('caption');
 
-        if (caption) {
-          const captionId = `table-caption-${i}`;
-          caption.setAttribute('id', captionId);
-          responsiveTable.setAttribute('aria-labelledby', captionId);
-        } else {
-          responsiveTable.setAttribute('aria-label', 'Table');
+          responsiveTable.classList.add('l-responsive-table');
+          responsiveTable.setAttribute('tabindex', '0');
+          responsiveTable.setAttribute('role', 'region');
+
+          if (caption) {
+            const captionId = `table-caption-${i}`;
+            caption.setAttribute('id', captionId);
+            responsiveTable.setAttribute('aria-labelledby', captionId);
+          } else {
+            responsiveTable.setAttribute('aria-label', 'Table');
+          }
+
+          responsiveTable.appendChild(table.cloneNode(true));
+          tableParent.replaceChild(responsiveTable, table);
         }
-
-        responsiveTable.appendChild(table.cloneNode(true));
-        table.parentNode.replaceChild(responsiveTable, table);
       });
     });
   },
