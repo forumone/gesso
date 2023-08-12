c09b17bbec93f23487ba8f7c74359547c2ce262f
KJ Monahan <kj@forumone.com>
2023-08-12 09:36:16 -0500

    Update finding of SCSS and JS files to be async with a shared cache

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const path = require('path');
-const glob = require('glob');
+const { Glob } = require('glob');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RemovePlugin = require('remove-files-webpack-plugin');
@@ -8,58 +8,61 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const embeddedSass = require('sass-embedded');
 
+async function gatherProjectFiles() {
+  const jsFiles = {};
+  const scssFiles = {};
+  const jsGlob = new Glob('source/**/!(*.stories).[jt]s', {
+    ignore: ['**/_*', 'source/@types/**'],
+  });
+  const scssGlob = new Glob('source/**/*.scss', jsGlob);
+  // The Airbnb style guide, which we're generally following, does not allow
+  // for for...of statements. There's a long debate about this rule at
+  // https://github.com/airbnb/javascript/issues/1271, but the gist seems to be
+  // that the intention is to disable loops for arrays in favor of array iteration.
+  // I don't think that applies here, since we're using it with an AsyncGenerator,
+  // and the code here follows the examples of the documentation for Glob and
+  // the MDN example of an AsyncGenerator.
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const currentFile of jsGlob.iterate()) {
+    const filePaths = currentFile.split(path.sep);
+    const sourceDirIndex = filePaths.indexOf('source');
+    if (sourceDirIndex >= 0) {
+      const fileName = path.basename(currentFile).replace(/\.[jt]s$/, '');
+      const newFilePath = `js/${fileName}`;
+      // Throw an error if duplicate files detected.
+      if (jsFiles[newFilePath]) {
+        throw new Error(`More than one file named ${fileName}.[jt]s found.`);
+      }
+      jsFiles[newFilePath] = {
+        import: path.resolve(__dirname, currentFile),
+      };
+    }
+  }
+  // See comment above for why this is disabled.
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const currentFile of scssGlob.iterate()) {
+    const filePaths = currentFile.split(path.sep);
+    const sourceDirIndex = filePaths.indexOf('source');
+    if (sourceDirIndex >= 0) {
+      const fileName = path.basename(currentFile, '.scss');
+      const newFilePath = `css/${fileName}`;
+      // Throw an error if duplicate files detected.
+      if (scssFiles[newFilePath]) {
+        throw new Error(`More that one file named ${fileName}.scss found.`);
+      }
+      scssFiles[newFilePath] = {
+        import: `./${currentFile}`,
+      };
+    }
+  }
+  return {
+    ...jsFiles,
+    ...scssFiles,
+  };
+}
+
 module.exports = {
-  entry: () => {
-    // Grab any JS/TS files.
-    const jsFiles = glob
-      .sync('source/**/!(*.stories).[jt]s', {
-        ignore: ['**/_*', 'source/@types/**'],
-      })
-      .reduce((entries, currentFile) => {
-        const filePaths = currentFile.split(path.sep);
-        const sourceDirIndex = filePaths.indexOf('source');
-        if (sourceDirIndex >= 0) {
-          const fileName = path.basename(currentFile).replace(/\.[jt]s$/, '');
-          const newFilePath = `js/${fileName}`;
-          // Throw an error if duplicate files detected.
-          if (entries[newFilePath]) {
-            throw new Error(
-              `More than one file named ${fileName}.[jt]s found.`
-            );
-          }
-          entries[newFilePath] = {
-            import: path.resolve(__dirname, currentFile),
-          };
-        }
-        return entries;
-      }, {});
-    // Grab any SCSS files that aren't prefixed with _.
-    const scssFiles = glob
-      .sync('source/**/*.scss', {
-        ignore: ['**/_*'],
-      })
-      .reduce((entries, currentFile) => {
-        const updatedEntries = entries;
-        const filePaths = currentFile.split(path.sep);
-        const sourceDirIndex = filePaths.indexOf('source');
-        if (sourceDirIndex >= 0) {
-          const fileName = path.basename(currentFile, '.scss');
-          const newFilePath = `css/${fileName}`;
-          // Throw an error if duplicate files detected.
-          if (updatedEntries[newFilePath]) {
-            throw new Error(`More that one file named ${fileName}.scss found.`);
-          }
-          updatedEntries[newFilePath] = {
-            import: `./${currentFile}`,
-          };
-        }
-        return updatedEntries;
-      }, {});
-    return {
-      ...jsFiles,
-      ...scssFiles,
-    };
-  },
+  entry: () => gatherProjectFiles(),
   plugins: [
     new MiniCssExtractPlugin(),
     new RemovePlugin({
@@ -67,8 +70,7 @@ module.exports = {
         test: [
           {
             folder: './dist/css',
-            method: absolutePath =>
-              new RegExp(/\.js(\.map)?$/, 'm').test(absolutePath),
+            method: absolutePath => /\.js(\.map)?$/m.test(absolutePath),
             recursive: true,
           },
         ],
@@ -175,5 +177,9 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
+    clean: {
+      keep: /images/,
+    },
   },
+  stats: 'minimal',
 };
