6dd6fd9ac15e196e3d10c525df7a77e293b0471c
Josh Wagner <jwagner@forumone.com>
2024-07-18 13:59:56 -0400

    [838] switch to using library

--- source/03-components/call-to-action/call-to-action.stories.jsx
+++ source/03-components/call-to-action/call-to-action.stories.jsx
@@ -3,6 +3,7 @@ import parse from 'html-react-parser';
 import { withGlobalWrapper } from '../../../.storybook/decorators';
 import twigTemplate from './call-to-action.twig';
 import data from './call-to-action.yml';
+import './call-to-action.scss';
 
 const settings = {
   title: 'Components/Call To Action',
