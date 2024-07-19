912d0d8e0a8ee790ac52b42b50fccad1cc8f979e
Josh Wagner <jwagner@forumone.com>
2024-07-19 16:15:16 -0400

    remove onload checking, destroy script once unloaded

--- .storybook/decorators.jsx
+++ .storybook/decorators.jsx
@@ -11,17 +11,14 @@ const gsapImport = (Story) => {
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     const script = document.createElement('script');
-    script.onload = () => {
-      setIsLoaded(true);
-    };
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js';
     document.body.appendChild(script);
     return () => {
-      // clean up effects of script here
+      document.body.removeChild(script);
     };
   }, []);
 
-  return isLoaded ? <Story /> : <div>Loading...</div>;
+  return <Story />;
 }
 
 export { withGlobalWrapper, gsapImport };
