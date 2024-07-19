7b2ccd0efb09e3a9deac1148ec39f469693d5a49
Josh Wagner <jwagner@forumone.com>
2024-07-19 16:14:50 -0400

    add checking to make sure library is actually loaded

--- source/03-components/card/card.es6.js
+++ source/03-components/card/card.es6.js
@@ -1,9 +1,24 @@
 import Drupal from 'drupal';
 
 Drupal.behaviors.cardAnimate = {
-  attach(context) {
+  attach() {
+    const callbackTimer = setInterval(() => {
+      let call = false;
+      try {
+        /* eslint-disable no-undef */
+        call = gsap.to('.c-card', { rotation: 27, x: 100, duration: 1 });
+        /* eslint-enable no-undef */
+      } catch (e) {
+        /* empty */
+      }
+
+      if (call) {
+        clearInterval(callbackTimer);
+      }
+    }, 100);
+
     setTimeout(() => {
-      gsap.to('.c-card', { rotation: 27, x: 100, duration: 1 });
+      clearInterval(callbackTimer);
     }, 1000);
   },
 };
