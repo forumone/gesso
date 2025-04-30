e7ad25c3343d83e8d93fd8c22e870927260bce9d
Dan Mouyard <dcmouyard@gmail.com>
2025-04-30 10:48:23 -0400

    Refactor how scrollbar width property is calculated

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,7 +11,6 @@ import './stubs/drupal';
 import './stubs/once';
 
 import '../dist/css/styles.css';
-import '../source/01-global/html-elements/00-universal/universal.es6';
 import '../source/01-global/html-elements/01-html/html.es6';
 
 function setupTwig(twig) {
--- source/01-global/html-elements/00-universal/universal.es6.js
+++
@@ -1,8 +0,0 @@
-import Drupal from 'drupal';
-import setScrollbarProperty from '../../../06-utility/_setScrollbarProperty.es6';
-
-Drupal.behaviors.setScrollbarProperty = {
-  attach() {
-    setScrollbarProperty();
-  },
-};
