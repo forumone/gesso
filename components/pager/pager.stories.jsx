d7da1040fcc5d38c64cc6f448ed2cc3a9bf626b1
KJ Monahan <kj@forumone.com>
2026-02-19 16:51:02 -0600

    Migrate pager to SDC

--- components/pager/pager.stories.jsx
+++ components/pager/pager.stories.jsx
@@ -1,13 +1,13 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
+import { withGlobalWrapper } from '../../.storybook/decorators.jsx';
 import twigTemplate from './pager.twig';
-import miniTwigTemplate from './pager--mini/pager--mini.twig';
+import miniTwigTemplate from '../pager-mini/pager-mini.twig';
 import data from './pager.yml';
-import miniData from './pager--mini/pager--mini.yml';
-import globalData from '../../00-config/storybook.global-data.yml';
-import './pager.scss';
-import './pager--mini/pager--mini.scss';
+import miniData from '../pager-mini/pager-mini.yml';
+import globalData from '../../source/00-config/storybook.global-data.yml';
+import './pager.source.scss';
+import '../pager-mini/pager-mini.source.scss';
 
 const settings = {
   title: 'Components/Pager',
