8e3dcbe0e1c96d7734294846ff3279dc9c399984
KJ Monahan <kj@forumone.com>
2026-02-19 16:47:32 -0600

    Migrate overlay-menu to SDC

--- components/overlay-menu/overlay-menu.stories.jsx
+++ components/overlay-menu/overlay-menu.stories.jsx
@@ -2,9 +2,9 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './overlay-menu.twig';
 import data from './overlay-menu.yml';
-import './overlay-menu.scss';
-import './overlay-menu.es6';
-import '../hamburger-button/hamburger-button.scss';
+import './overlay-menu.source.scss';
+import './overlay-menu.source.js';
+import '../../source/03-components/hamburger-button/hamburger-button.scss';
 
 const settings = {
   title: 'Components/Menu/Overlay Menu',
