901c716383f963ed1dcb3a269a62d167131018d5
KJ Monahan <kj@forumone.com>
2026-02-19 16:58:42 -0600

    Migrate read-more-link to SDC

--- components/read-more-link/read-more-link.stories.jsx
+++ components/read-more-link/read-more-link.stories.jsx
@@ -1,13 +1,13 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
+import { withGlobalWrapper } from '../../.storybook/decorators.jsx';
 import twigTemplate from './read-more-link.twig';
-import globalData from '../../00-config/storybook.global-data.yml';
+import globalData from '../../source/00-config/storybook.global-data.yml';
 import data from './read-more-link.yml';
-import './read-more-link.scss';
+import './read-more-link.source.scss';
 // Importing IconLink to ensure its assets gets loaded in Storybook when this
 // story is referenced since Drupal loads them as a library.
-import '../icon-link/icon-link.stories.jsx';
+import '../../components/icon-link/icon-link.stories.jsx';
 
 const settings = {
   title: 'Components/Read More Link',
