29765e3b521b9c668924aacad7ec6a66f7be7c1e
KJ Monahan <kj@forumone.com>
2026-02-20 10:26:24 -0600

    Migrate site-slogan to SDC

--- components/site-slogan/site-slogan.stories.jsx
+++ components/site-slogan/site-slogan.stories.jsx
@@ -1,8 +1,8 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
+import { withGlobalWrapper } from '../../.storybook/decorators.jsx';
 import twigTemplate from './site-slogan.twig';
-import globalData from '../../00-config/storybook.global-data.yml';
+import globalData from '../../source/00-config/storybook.global-data.yml';
 
 const settings = {
   title: 'Components/Site Slogan',
