9f762157aa6be724796f81a1e3373c3b34853b85
KJ Monahan <kj@forumone.com>
2026-02-20 10:20:56 -0600

    Migrate side-menu to SDC

--- components/side-menu/side-menu.stories.jsx
+++ components/side-menu/side-menu.stories.jsx
@@ -2,9 +2,9 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './side-menu.twig';
 import data from './side-menu.yml';
-import './side-menu.scss';
-import './side-menu.es6';
-import '../hamburger-button/hamburger-button.scss';
+import './side-menu.source.scss';
+import './side-menu.source.ts';
+import '../../source/03-components/hamburger-button/hamburger-button.scss';
 
 const settings = {
   title: 'Components/Menu/Side Menu',
