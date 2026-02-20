bb6af4de91b16ae429345c949c2d8669a90fcefd
KJ Monahan <kj@forumone.com>
2026-02-20 10:18:09 -0600

    Migrate rss-feed to SDC

--- components/rss-feed/rss-feed.stories.jsx
+++ components/rss-feed/rss-feed.stories.jsx
@@ -1,13 +1,13 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
+import { withGlobalWrapper } from '../../.storybook/decorators.jsx';
 import twigTemplate from './rss-feed.twig';
-import globalData from '../../00-config/storybook.global-data.yml';
+import globalData from '../../source/00-config/storybook.global-data.yml';
 import data from './rss-feed.yml';
-import './rss-feed.scss';
+import './rss-feed.source.scss';
 // Importing IconLink to ensure its CSS/JS gets loaded in Storybook when this
 // story is referenced.
-import { IconLink } from '../icon-link/icon-link.stories.jsx';
+import { IconLink } from '../../components/icon-link/icon-link.stories.jsx';
 
 const settings = {
   title: 'Components/RSS Feed',
