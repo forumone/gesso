e8273bbdcc4beb2b6e0a0eaad0d21d8ed20c607b
KJ Monahan <kj@forumone.com>
2026-02-20 12:58:22 -0600

    Migrate nav

--- components/layouts/nav/nav.stories.jsx
+++ components/layouts/nav/nav.stories.jsx
@@ -4,7 +4,7 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './nav.twig';
 import data from './nav.yml';
-import ContentPlaceholder from '../../01-global/content-placeholder/content-placeholder';
+import ContentPlaceholder from '../../source/01-global/content-placeholder/content-placeholder';
 
 const settings = {
   title: 'Layouts/Nav',
