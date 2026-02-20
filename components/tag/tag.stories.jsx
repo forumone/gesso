38dc7d011da5b6c7e0612d374c09352938c7cda4
KJ Monahan <kj@forumone.com>
2026-02-20 10:44:55 -0600

    Migrate tag to SDC

--- components/tag/tag.stories.jsx
+++ components/tag/tag.stories.jsx
@@ -1,9 +1,9 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
+import { withGlobalWrapper } from '../../.storybook/decorators.jsx';
 import twigTemplate from './tag.twig';
 import data from './tag.yml';
-import './tag.scss';
+import './tag.source.scss';
 
 const settings = {
   title: 'Components/Tag',
