e9f6f64dd01fe6438b2640547ae0e5693b2ebd43
KJ Monahan <kj@forumone.com>
2026-02-20 13:09:07 -0600

    Migrate site-container

--- components/layouts/site-container/site-container.stories.jsx
+++ components/layouts/site-container/site-container.stories.jsx
@@ -4,7 +4,7 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './site-container.twig';
 import data from './site-container.yml';
-import ContentPlaceholder from '../../01-global/content-placeholder/content-placeholder';
+import ContentPlaceholder from '../../source/01-global/content-placeholder/content-placeholder';
 
 const settings = {
   title: 'Layouts/Site Container',
